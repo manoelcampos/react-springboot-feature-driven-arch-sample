416ff169dacc8572f3403c11bad2b4144baf2a8b
Manoel Campos <manoelcampos@gmail.com>
2025-12-10 08:42:42 -0300

    docs(backend): Add docs to HttpError and translate previous ones.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- backend/src/main/java/sample/application/api/shared/exception/HttpError.java
+++ backend/src/main/java/sample/application/api/shared/exception/HttpError.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
- * Representa uma mensagem de erro HTTP com se devido código de stats.
- * @param status código de status HTTP
- * @param description Descrição do código de status HTTP
- * @param message Mensagem de erro
+ * Represents an HTTP error message with its corresponding status code.
+ * @param status HTTP status code
+ * @param description Description of the HTTP status code
+ * @param message Error message
  * @author Manoel Campos
  */
 public record HttpError(int status, String description, String message) {
@@ -19,8 +19,16 @@ public record HttpError(int status, String description, String message) {
         this(status.value(), status.name(), message);
     }
 
+    /**
+     * {@return the http status name from an HTTP error message}
+     * If the error message is "404 Not Found", it returns "Not Found".
+     * @param ex the exception containing the HTTP error message
+     */
     private static String getHttpStatusName(final ResponseStatusException ex) {
         final String[] errorMsgParts = ex.getMessage().split(" ");
-        return errorMsgParts.length == 0 ? "" : errorMsgParts[1];
+        if(errorMsgParts.length < 2)
+            return "";
+
+        return errorMsgParts[1];
     }
 }
